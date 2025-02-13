be6e2fa7e70fbef3a857b7598759108822f54c84
jsuchanun15 <j.suchanan15@gmail.com>
2025-02-14 00:03:53 +0700

    Update lab19.h

--- lab19.h
+++ lab19.h
@@ -63,6 +63,44 @@ void Unit::newTurn(){
 	guard_on = false;
 }
 
+bool Unit::isDead(){
+    if(hp<=0) return true;
+    else return false;
+}
+
+void Unit::guard(){
+    guard_on = true;
+}
+
+int Unit::beAttacked(int oppatk){
+        int dmg;
+        if(guard_on == false){
+            dmg = oppatk - def;
+            hp = hp - dmg;
+        }else if(guard_on == true){
+            dmg = (oppatk - def)/3;
+            hp = hp - dmg;
+        }
+        return dmg;
+
+
+
+}
+
+int Unit::attack(Unit &oppatk){
+	return oppatk.beAttacked(atk);
+}
+
+int Unit::heal(){
+    int x;
+    x = rand()%21+10 ;
+    if(hp+x >= hpmax){
+        x = hpmax - hp;
+        hp = hpmax;
+    }
+    else hp = hp + x;
+    return x;
+};
 
 
 /////////////////////////////////////////////////////////////////////////////////////
